70c538ad23cb61e95d7b14f1811a27c8b985ffa1
zz13019192566 <13019192566@163.com>
2019-05-17 15:14:50 +0800

    新拉瓦

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,30 +24,57 @@
 				</div>
 				<ul class="layui-nav layui-nav-tree" lay-filter="leftNav">
 				  <li class="layui-nav-item layui-nav-itemed">
-				    <a href="javascript:;"><i class="iconfont">&#xe607;</i>菜单管理</a>
+				    <a href="javascript:;"><i class="iconfont">&#xe607;</i>管理员管理</a>
 				    <dl class="layui-nav-child">
-				      <dd><a href="javascript:;" data-url="menu1.html" data-id='1' data-text="后台菜单"><span class="l-line"></span>后台菜单</a></dd>
-				      <dd><a href="javascript:;" data-url="menu2.html" data-id='2' data-text="前台菜单"><span class="l-line"></span>前台菜单</a></dd>
+				      <dd><a href="javascript:;" data-url="menu1.html" data-id='1' data-text="后台菜单"><span class="l-line"></span>浏览管理员</a></dd>
+				      <dd><a href="javascript:;" data-url="menu2.html" data-id='2' data-text="前台菜单"><span class="l-line"></span>添加管理员</a></dd>
+				    </dl>
+				  </li>
+				  <li class="layui-nav-item layui-nav-itemed">
+				    <a href="javascript:;"><i class="iconfont">&#xe607;</i>用户管理</a>
+				    <dl class="layui-nav-child">
+				      <dd><a href="javascript:;" data-url="menu1.html" data-id='1' data-text="后台菜单"><span class="l-line"></span>浏览用户</a></dd>
+				    </dl>
+				  </li>
+				   <li class="layui-nav-item layui-nav-itemed">
+				    <a href="javascript:;"><i class="iconfont">&#xe607;</i>分类管理</a>
+				    <dl class="layui-nav-child">
+				      <dd><a href="javascript:;" data-url="menu1.html" data-id='1' data-text="后台菜单"><span class="l-line"></span>分类信息</a></dd>
+				      <dd><a href="javascript:;" data-url="menu1.html" data-id='1' data-text="后台菜单"><span class="l-line"></span>添加分类</a></dd>
+
 				    </dl>
 				  </li>
 				  <li class="layui-nav-item">
-				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>内容管理</a>
+				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>帖子管理</a>
 				    <dl class="layui-nav-child">
-				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>文章管理</a></dd>
-				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>单页管理</a></dd>
+				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>帖子浏览</a></dd>
+				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>添加帖子</a></dd>
+				    </dl>
+				     <li class="layui-nav-item">
+				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>敏感词管理</a>
+				    <dl class="layui-nav-child">
+				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>敏感词浏览</a></dd>
+				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>添加敏感词</a></dd>
 				    </dl>
 				  </li>
-				  <li class="layui-nav-item">
-				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>管理员管理</a>
+				   <li class="layui-nav-item">
+				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>轮播图管理</a>
 				    <dl class="layui-nav-child">
-				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>浏览管理员</a></dd>
-				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>添加管理员管理</a></dd>
+				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>轮播图浏览</a></dd>
+				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>添加轮播图</a></dd>
 				    </dl>
+				  </li>
+				   <li class="layui-nav-item">
+				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>公告管理</a>
+				    <dl class="layui-nav-child">
+				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>公告浏览</a></dd>
+				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>添加公告</a></dd>
+				    </dl>
+				  </li>
 				  <li class="layui-nav-item">
 				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>友情链接</a>
 				    <dl class="layui-nav-child">
-				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>文章管理</a></dd>
-				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>单页管理</a></dd>
+				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>查看链接</a></dd>
 				    </dl>
 				  <li class="layui-nav-item">
 				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>RBAC</a>
@@ -59,8 +86,7 @@
 				  <li class="layui-nav-item">
 				    <a href="javascript:;"><i class="iconfont">&#xe608;</i>个人信息</a>
 				    <dl class="layui-nav-child">
-				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>文章管理</a></dd>
-				      <dd><a href="javascript:;" data-url="danye-list.html" data-id='9' data-text="单页管理"><span class="l-line"></span>单页管理</a></dd>
+				      <dd><a href="javascript:;" data-url="article-list.html" data-id='3' data-text="文章管理"><span class="l-line"></span>修改个人信息</a></dd> 
 				    </dl>
 				  </li>
 				  <li class="layui-nav-item">
